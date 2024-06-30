d8d555033e2f747743fd8c5e76781ee3eb86b3fb
Denni <dennipes1@gmail.com>
2024-06-30 19:26:11 +0700

    make navbar, sidebar, feed, and recommendations

---
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import './PlayVideo.css'
+import video1 from '../../assets/video.mp4'
+import like from '../../assets/like.png'
+import dislike from '../../assets/dislike.png'
+import share from '../../assets/share.png'
+import save from '../../assets/save.png'
+import jack from '../../assets/jack.png'
+import user_profile from '../../assets/user_profile.jpg'
+
+const PlayVideo = () => {
+  return (
+    <div className='play-video'>
+        <video src={video1} controls autoPlay muted></video>
+        <h3>Best Youtube Channel to learn code</h3>
+        <div className="play-video-info">
+            <p>1525 Views &bull; 2 days ago</p>
+            <div>
+                <span><img src={like} alt="" /> 125</span>
+                <span><img src={dislike} alt="" /> 2</span>
+                <span><img src={share} alt="" /> Share</span>
+                <span><img src={save} alt="" /> Save</span>
+            </div>
+        </div>
+        <hr />
+        <div className="publisher">
+            <img src={jack} alt="" />
+            <div>
+                <p>Denni</p>
+                <span>1M Subscribers</span>
+            </div>
+            <button>Subscribe</button>
+        </div>
+        <div className="vid-description">
+            <p>Channel Makes leanring ease</p>
+            <p>Subscribe great stack to</p>
+            <hr />
+            <h4>130 comments</h4>
+            <div className="comment">
+                <img src={user_profile} alt="" />
+                <div>
+                    <h3>Nicobal Jackson <span>1 day ago</span></h3>
+                    <p>A Global computer netwrok Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+                    <div className="comment-action">
+                        <img src={like} alt="" />
+                        <span>244</span>
+                        <img src={dislike} alt="" />
+                    </div>
+                </div>
+            </div>
+            <div className="comment">
+                <img src={user_profile} alt="" />
+                <div>
+                    <h3>Nicobal Jackson <span>1 day ago</span></h3>
+                    <p>A Global computer netwrok Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+                    <div className="comment-action">
+                        <img src={like} alt="" />
+                        <span>244</span>
+                        <img src={dislike} alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default PlayVideo
